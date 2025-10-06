4420b0cfd0b1d778697cf3cae9e6b4027881e552
Himanshu Paliwal <hpaliwal364@gmail.com>
2025-10-06 07:42:04 +0530

    integrate fetch workspace api

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,20 @@
-import React, { memo } from 'react';
+import React, { memo, useEffect } from 'react';
 
 import UserButton from '@/components/atoms/UserButton/UserButton';
+import { useFetchWorkspace } from '@/hooks/apis/workspaces/useFetchWorkspace';
 
 const Home: React.FC = () => {
+  const { isFetching, workspaces } = useFetchWorkspace();
+
+  useEffect(() => {
+    if (isFetching) return;
+    console.log('workspaces in home', workspaces);
+
+    if (workspaces.length === 0 || !workspaces) {
+      console.log('No workspaces found, create one');
+    }
+  }, [isFetching, workspaces]);
+
   return (
     <div className="">
       <h1>Home</h1>
