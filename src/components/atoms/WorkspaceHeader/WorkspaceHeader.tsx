ead28f3231a2fff928bde800174cc19d10bde732
Himanshu Paliwal <hpaliwal364@gmail.com>
2025-11-13 12:44:16 +0530

    updated the workspace header component

--- src/components/atoms/WorkspaceHeader/WorkspaceHeader.tsx
+++ src/components/atoms/WorkspaceHeader/WorkspaceHeader.tsx
@@ -2,18 +2,25 @@ import { ArrowLeft } from 'lucide-react';
 import React, { memo } from 'react';
 
 import { Button } from '@/components/ui/button';
+import { useOpenDrawer } from '@/hooks/context/useOpenDrawer';
 
 interface IProps {
   title: string;
 }
 
 const WorkspaceHeader: React.FC<IProps> = ({ title }) => {
+  const { setOpenOpenDrawer } = useOpenDrawer();
+
   return (
     <div className="bg-white border-b h-[50px] flex items-center px-4 overflow-hidden">
-      <Button variant={'transparent'} className="sm:hidden flex !p-1">
+      <Button
+        variant={'transparent'}
+        className="sm:hidden flex !p-1"
+        onClick={() => setOpenOpenDrawer(true)}
+      >
         <ArrowLeft className="size-5" color="black" />
       </Button>
-      <p className="text-lg font-semibold">{title}</p>
+      <p className="text-lg font-semibold sm:px-0 px-2">{title}</p>
     </div>
   );
 };
